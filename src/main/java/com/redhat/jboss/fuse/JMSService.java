8e4e9d06c49f4984b21073d1f03ef9aeab4a3333
Eugene <euaudev@gmail.com>
2017-11-13 10:08:57 -0500

    Updated

---
+++ src/main/java/com/redhat/jboss/fuse/JMSService.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.redhat.jboss.fuse;
+
+import javax.annotation.Resource;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+
+
+public class JMSService {
+	/**
+	 * 
+	 */
+
+	@Resource(name = "java:jboss/camel/context/myCamel")
+	private CamelContext camelContext;
+
+	protected String getHelloName( String name){
+        ProducerTemplate producer = camelContext.createProducerTemplate();
+        String result = producer.requestBody("direct:start", name, String.class);
+    	System.out.print(result);
+    	return result;
+    }
+}
